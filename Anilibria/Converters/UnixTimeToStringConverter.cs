1e64708cc8289e01b454584bd282260eca2f913f
Rus Sharafiev <srr@live.ru>
2023-12-08 15:19:34 +0300

    Add unix time to string converter

---
+++ Anilibria/Converters/UnixTimeToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anilibria.Core.Models;
+using Microsoft.UI.Xaml.Data;
+
+namespace Anilibria.Converters;
+public class UnixTimeToStringConverter : IValueConverter
+{
+    #region IValueConverter Members
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var seconds = (long)value;
+        return DateTimeOffset.FromUnixTimeSeconds(seconds).ToString("G");
+    }
+
+    public object ConvertBack(object value, Type targetType,
+        object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
