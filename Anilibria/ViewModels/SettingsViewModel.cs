2d2b62021d1f07b21c30ad59823b425b1099fdad
Rus Sharafiev <srr@live.ru>
2023-12-25 23:54:03 +0300

    Add settings cards

--- Anilibria/ViewModels/SettingsViewModel.cs
+++ Anilibria/ViewModels/SettingsViewModel.cs
@@ -5,10 +5,9 @@ using Anilibria.Contracts.Services;
 using Anilibria.Helpers;
 
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 using Microsoft.UI.Xaml;
-
+using Microsoft.UI.Xaml.Controls;
 using Windows.ApplicationModel;
 
 namespace Anilibria.ViewModels;
@@ -21,28 +20,24 @@ public partial class SettingsViewModel : ObservableRecipient
     private ElementTheme _elementTheme;
 
     [ObservableProperty]
-    private string _versionDescription;
+    private int _elementThemeIndex;
 
-    public ICommand SwitchThemeCommand
-    {
-        get;
-    }
+    [ObservableProperty]
+    private string _versionDescription;
 
     public SettingsViewModel(IThemeSelectorService themeSelectorService)
     {
         _themeSelectorService = themeSelectorService;
         _elementTheme = _themeSelectorService.Theme;
         _versionDescription = GetVersionDescription();
+        _elementThemeIndex = _elementTheme switch
+        {
+            ElementTheme.Light => 0,
+            ElementTheme.Dark => 1,
+            _ => 2
+        };
 
-        SwitchThemeCommand = new RelayCommand<ElementTheme>(
-            async (param) =>
-            {
-                if (ElementTheme != param)
-                {
-                    ElementTheme = param;
-                    await _themeSelectorService.SetThemeAsync(param);
-                }
-            });
+        System.Diagnostics.Debug.WriteLine(_elementThemeIndex);
     }
 
     private static string GetVersionDescription()
@@ -62,4 +57,23 @@ public partial class SettingsViewModel : ObservableRecipient
 
         return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
     }
+
+    #region ThemeChange
+    public async void Theme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var selectedTheme = e.AddedItems.First();
+        if (sender is ComboBox comboBox)
+        {
+            var currentTheme = comboBox.SelectedIndex switch
+            {
+                0 => ElementTheme.Light,
+                1 => ElementTheme.Dark,
+                _ => ElementTheme.Default,
+            };
+
+            if (currentTheme != ElementTheme) 
+                await _themeSelectorService.SetThemeAsync(currentTheme);
+        }
+    }
+    #endregion
 }
